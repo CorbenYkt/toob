5b3a511f42210830460df0b3285933b2d4657123
CorbenYkt <117908636+CorbenYkt@users.noreply.github.com>
2025-03-04 16:54:23 +1300

    UPD: minor

--- src/pages/productselection/index.tsx
+++ src/pages/productselection/index.tsx
@@ -3,9 +3,9 @@ import CheckoutForm from "@/pages/checkout";
 import ImageDisplay from "@/components/imagedisplay";
 
 const products = {
-    "Dust pink": ["./img/goods/1/dust_pink_1.png", "./img/goods/1/dust_pink_2.png"],
     "Neon pink": ["./img/goods/1/neon_pink_1.png"],
     "Lime Green": ["./img/goods/1/lime_green_1.png"],
+    "Dust pink": ["./img/goods/1/dust_pink_1.png", "./img/goods/1/dust_pink_2.png"],
     "Navy Blue": ["./img/goods/1/navy_blue_1.png", "./img/goods/1/navy_blue_2.png"],
     "Orange": ["./img/goods/1/orange_1.png", "./img/goods/1/orange_2.png"]
 };
@@ -19,7 +19,7 @@ const ProductSelection: React.FC = () => {
         <div className="max-w-3xl mx-auto p-4">
             <h2 className="text-2xl font-bold">Buy toob</h2><br></br>
             <div className="flex flex-col">
-                <div className="h-96 flex items-start">
+                <div className="flex items-start">
                     <div className="max-w-1/2">
                         <img className="w-full object-contain border rounded-lg" src={mainImage} alt="" />
                         <br></br>
@@ -36,28 +36,25 @@ const ProductSelection: React.FC = () => {
                                 <option key={color} value={color}>{color}</option>
                             ))}
                         </select>
-
                     </div>
-                    <div className="flex flex-col  ml-4 text-gray-800 text-justify">
+                    <div className="flex flex-col ml-4 text-gray-800 text-justify">
                         <div className="flex flex-wrap gap-2">
-                            {Object.values(products).flat().map((img, index) => (
+                            {Object.values(products).flat().map((src, index) => (
                                 <img key={index}
                                     className="w-12 h-auto object-cover rounded-lg cursor-pointer border hover:border-[#F25826]"
-                                    src={img}
+                                    src={src}
                                     alt=""
-                                    onClick={() => setMainImage(img)}
+                                    onClick={() => setMainImage(src)}
                                 />
                             ))}
-                        </div><br></br>
-                        <p>TOOB blankets for child bike seats - handmade in Wellington</p><br></br>
-
-                        <p>Specifications
-                            Outer material: waterproof and windproof high-quality PUL (PolyUrethane Laminate)
-
-                            Inner material: thick arctic fleece
-
-                            Reflective tape on the back
-
+                        </div>
+                        <br></br>
+                        <p>TOOB blankets for child bike seats - handmade in Wellington</p>
+                        <br></br>
+                        <p>Specifications:<br></br>
+                            Outer material: waterproof and windproof high-quality PUL (PolyUrethane Laminate)<br></br>
+                            Inner material: thick arctic fleece<br></br>
+                            Reflective tape on the back<br></br>
                             Size: one size (approx 57X84cm) fits most rear-mounted child seats</p><br></br>
                         <button
                             className="bg-[#F9EAD7] text-[#F25826] px-4 py-2 rounded self-start font-bold"
@@ -72,7 +69,7 @@ const ProductSelection: React.FC = () => {
 
             {showCheckout && (
                 <div
-                    className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
+                    className="fixed inset-0 bg-gray-600/50 flex justify-center items-center z-50"
                     onClick={() => setShowCheckout(false)}
                 >
                     <div
